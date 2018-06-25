1435e64b2d8838a6d0006cfcd4c83e4dc059139b
TAJD <tom0dickson@gmail.com>
2018-06-25 12:08:46 +0100

    working tests

--- test/aerodynamics.jl
+++ test/aerodynamics.jl
@@ -2,5 +2,5 @@ c_awa = sail_route.awa(60, 3.086, 5.144)
 # @test isapprox(0.6669807044553968, c_awa, rtol=3)
 
 @testset "Test apparent wind angle" begin
-    @test isapprox(1.0, c_awa, rtol=3)
+    @test isapprox(0.6669807044553968, c_awa, rtol=3)
 end
