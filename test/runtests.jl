4519ec071fd933a29521ef98987f968f38289e24
TAJD <tom0dickson@gmail.com>
2018-06-25 12:14:10 +0100

    tests using relative package import

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
-include("/Users/thomasdickson/Documents/sail_route.jl/src/sail_route.jl")
+# push!(LOAD_PATH,"./src/")
+#
+# include("/Users/thomasdickson/Documents/sail_route.jl/src/sail_route.jl")
 
-# using sail_route
+using sail_route
 
 using Test
-include("aerodynamics.jl")
+print("starting tests")
+@time include("aerodynamics.jl")
