3b0949b1a2ea9dd141cb281b8d56c6d1ad2f5d23
Tom Dickson <tom0dickson@gmail.com>
2018-08-22 09:40:31 +0100

    Initial weather routing implementation

--- sail_route/src/performance/failure_model/failure.jl
+++ sail_route/src/performance/failure_model/failure.jl
@@ -1,15 +1,12 @@
 using PyCall
 
-@pyimport importlib.machinery as machinery
-loader = machinery.SourceFileLoader("failure",ENV["HOME"]*"/sail_route.jl/sail_route/src/performance/failure_model/failure.py")
-fail_models = loader[:load_module]("failure")
+# @pyimport importlib.machinery as machinery
+# loader = machinery.SourceFileLoader("failure",ENV["HOME"]*"/sail_route.jl/sail_route/src/performance/failure_model/failure.py")
+# fail_models = loader[:load_module]("failure")
 
-function load_env_failure_model()
-    return fail_models[:gen_env_model]()
-end
 
-function interrogate_model(bp, tws, twa, h, theta)
-    return fail_models[:env_bbn_interrogate](bp, tws, twa, h, theta)
+function interrogate_model(performance, t, tws, twa, h, theta)
+    return 0.0
 end
 
 # include poisson failure function here 
--- sail_route/src/weather/load_weather.jl
+++ sail_route/src/weather/load_weather.jl
@@ -9,3 +9,16 @@ function sample_weather()
     wisp, widi, cusp, cudi, wahi, wadi = w[:sample_weather_scenario]()
     return wisp, widi, cusp, cudi, wahi, wadi
 end
+
+
+"Return the wave height and direction and the wind speed and direction from an ERA5 weather file."
+function process_era5_weather(path_nc, longs, lats)
+    rg_wisp, rg_widi, rg_wh, rg_wd, rg_wp = w[:process_era5_weather](path_nc, longs, lats)
+    return rg_wisp, rg_widi, rg_wh, rg_wd, rg_wp
+end
+
+
+function load_era5_weather(path_nc)
+    wisp, widi, wh, wd, wp = w[:retrieve_era5_weather](path_nc)
+    return wisp, widi, wh, wd, wp
+end
