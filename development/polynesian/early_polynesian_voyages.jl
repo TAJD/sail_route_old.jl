3691f115fc8ac18202e4bff2580b8ab43d05ac81
Tom Dickson <tom0dickson@gmail.com>
2018-12-04 13:52:23 +0000

    early colonisation voyage simulation settings working

---
+++ development/polynesian/early_polynesian_voyages.jl
@@ -0,0 +1,75 @@
+using Distributed
+@everywhere begin
+    # include(ENV["HOME"]*"/sail_route_old/src/weather/load_weather.jl")
+    # include(ENV["HOME"]*"/sail_route_old/src/route/domain.jl")
+    # include(ENV["HOME"]*"/sail_route_old/src/route/shortest_path.jl")
+    include(ENV["HOME"]*"/sail_route_old/src/sail_route.jl")
+    include(ENV["HOME"]*"/sail_route_old/development/sensitivity/discretization_error.jl")
+    include(ENV["HOME"]*"/sail_route_old/development/polynesian/polynesian_sims_utils.jl")
+    using ParallelDataTransfer
+    using BenchmarkTools
+    using Printf
+    using Dates
+    using CSV
+    using DataFrames
+    using SharedArrays
+    # import sail_route
+
+
+    route_solve_shared_chunk!(results, times, perfs, route, time_indexes, x, y, wisp, widi, wadi, wahi, cusp, cudi) = route_solve_chunk!(results, myrange(results)..., times, perfs, route, time_indexes, x, y, wisp, widi, wadi, wahi, cusp, cudi)
+
+    """parallized_uncertain_routing(save_path, lon1, lat1, lon2, lat2,
+                                    weather, times, perf_t, min_dist)
+    Parallized routing simulations.
+    """
+    function parallized_uncertain_routing(save_path, lon1, lat1, lon2, lat2,
+                                          weather, times, perf_t, min_dist)
+        sim_times = [DateTime(t) for t in times]
+        twa, tws, perf = perf_t
+        @show params = [round(i; digits=2) for i in LinRange(0.50, 1.50, 21)]
+        @show polar = sail_route.setup_perf_interpolation(tws, twa, perf)
+        wave_resistance_model = sail_route.typical_aerrtsen()
+        perfs = sail_route.generate_performance_uncertainty_samples(polar, params, wave_resistance_model)
+        results = SharedArray{Float64, 2}(length(sim_times), length(perfs))
+        save_path = ENV["HOME"]*"/sail_route_old/development/polynesian"*save_path
+        println(save_path)
+        @show n = sail_route.calc_nodes(lon1, lon2, lat1, lat2, min_dist)
+        route = sail_route.Route(lon1, lon2, lat1, lat2, n, n)
+        wisp, widi, wahi, wadi, wapr, time_indexes = sail_route.load_era20_weather(weather)
+        x, y, wisp, widi, wadi, wahi = sail_route.generate_inputs(route, wisp, widi, wadi, wahi)
+        dims = size(wisp)
+        cusp, cudi = sail_route.return_current_vectors(y, dims[1])
+        @sync begin
+            @show for p in procs(results)
+                @async remotecall_wait(route_solve_shared_chunk!, p, results, times,
+                                       perfs, route, time_indexes, x, y,
+                                       wisp, widi, wadi, wahi, cusp, cudi)
+            end
+        end
+        _results = DataFrame(results)
+        names!(_results, [Symbol(i) for i in params])
+        insert!(_results, 1, times, :start_time)
+        CSV.write(save_path, _results)
+        return _results
+    end
+end
+
+
+"""Run polynesian simulations based on arguments from command line. Two arguments used to set the lower and upper limits of a range to iterate over."""
+function run_simulations(i)
+    @everywhere settings = generate_early_voyaging_settings()
+    @everywhere vals = settings[1]
+    @everywhere paths = settings[2]
+    @everywhere path = paths[i]
+    @everywhere s = vals[i]
+    parallized_uncertain_routing(path, s[2], s[4], s[3], s[5], s[6], s[7], s[8], s[1])
+end
+
+# comment out these lines if running from bash script
+if isempty(ARGS) == false
+    @show i = parse(Int64, ARGS[1]); sendto(workers(), i=i)
+end
+
+# i = 3; sendto(workers(), i=i)
+
+run_simulations(i)
--- development/polynesian/polynesian_sims_utils.jl
+++ development/polynesian/polynesian_sims_utils.jl
@@ -1,4 +1,4 @@
-using Distributed, SharedArrays, CSV, Interpolations
+using Distributed, SharedArrays, CSV, Interpolations, Dates
 
 
 """Create a custom iterator which breaks up a range based on the processor number"""
@@ -85,8 +85,8 @@ function disc_routing_analysis(lon2, lon1, lat2, lat1, perf, time,
 end
 
 
-"""Generate the parameters to simulate for all cases of the Polynesian routing simulation."""
-function generate_settings()
+"""Generate the parameters to simulate for Polynesian colonisation voyages."""
+function generate_colonisation_voyage_settings()
     start_locations_lat = [-13.917, -21.21]
     start_locations_lon = [-171.75, -175.15]
     finish_locations_lat = [-19.59, -17.53]
@@ -96,8 +96,8 @@ function generate_settings()
     boat_performance = [load_tong(), load_boeckv2()]
     boat_performance_names = ["/tongiaki/", "/boeckv2/"]
     t_inc = 12
-    t_low = Dates.DateTime(1976, 1, 1, 0, 0, 0):Dates.Hour(t_inc):Dates.DateTime(1976, 11, 1, 0, 0, 0)
-    t_high = Dates.DateTime(1982, 1, 1, 0, 0, 0):Dates.Hour(t_inc):Dates.DateTime(1982, 11, 1, 0, 0, 0)
+    t_low = Dates.DateTime(1976, 1, 1, 0, 0, 0):Dates.Hour(t_inc):Dates.DateTime(1976, 12, 31, 0, 0, 0)
+    t_high = Dates.DateTime(1982, 1, 1, 0, 0, 0):Dates.Hour(t_inc):Dates.DateTime(1982, 12, 31, 0, 0, 0)
     weather_times = [t_low, t_high]
     weather_names = ["low", "high"]
     weather_paths = [ENV["HOME"]*"/weather_data/polynesia_weather/low/1976/1976_polynesia.nc",
@@ -124,6 +124,49 @@ function generate_settings()
     end
     return simulation_settings, save_paths
 end
+
+
+"""Generate the parameters to simulate early voyages."""
+function generate_early_voyaging_settings()
+    start_locations_lat = [-9.467, -17.733, -18.167, 13.917, -21.21]
+    start_locations_lon = [-159.817, -168.317, -178.45, -171.75, -175.15]
+    finish_locations_lat = [-17.733, -18.167, -13.917, -21.21]
+    finish_locations_lon = [-168.317, -178.45, -171.75, -175.15]
+    start_location_names = ["solomons", "vanuatu", "fiji", "fiji"]
+    finish_location_names = ["vanuatu", "fiji", "upolu", "tonga"]
+    boat_performance = [load_tong(), load_boeckv2(), load_boeckv2()]  #need to change this line here 
+    boat_performance_names = ["/tongiaki/", "/boeckv2/", "/hokolua/"]
+    t_inc = 12
+    t_low = Dates.DateTime(1976, 1, 1, 0, 0, 0):Dates.Hour(t_inc):Dates.DateTime(1976, 12, 31, 0, 0, 0)
+    t_mid = Dates.DateTime(1985, 1, 1, 0, 0, 0):Dates.Hour(t_inc):Dates.DateTime(1985, 12, 31, 0, 0, 0)
+    t_high = Dates.DateTime(1982, 1, 1, 0, 0, 0):Dates.Hour(t_inc):Dates.DateTime(1982, 12, 31, 0, 0, 0)
+    weather_times = [t_low, t_mid, t_high]
+    weather_names = ["low", "mid", "high"]
+    weather_paths = [ENV["HOME"]*"/weather_data/polynesia_weather/low/1976/polynesia_1976.nc",
+                     ENV["HOME"]*"/weather_data/polynesia_weather/med/1985/polynesia_1985.nc",
+                     ENV["HOME"]*"/weather_data/polynesia_weather/high/1982/polynesia_1982.nc"]
+    node_spacings = [20.0, 15.0, 10.0]
+    simulation_settings = []
+    save_paths = []
+    for i in eachindex(node_spacings)
+        for j in eachindex(start_location_names)
+            for l in eachindex(weather_names)
+                for m in eachindex(boat_performance_names)
+                    push!(simulation_settings, [node_spacings[i], start_locations_lon[j], finish_locations_lon[j],
+                                                start_locations_lat[j], finish_locations_lat[j],
+                                                weather_paths[l], weather_times[l],
+                                                boat_performance[m]])
+                    route_name = start_location_names[j]*"_to_"*finish_location_names[j]
+                    path = boat_performance_names[m]*"_routing_"*route_name*"_"*repr(weather_times[l][1])*"_to_"*repr(weather_times[l][end])*"_"*repr(node_spacings[i])*"_nm.txt"
+                    push!(save_paths, path)
+                end
+            end
+        end
+    end
+    return simulation_settings, save_paths
+end
+
+simulation_settings, save_paths = generate_early_voyaging_settings()
 
 
 """Generate the parameters to simulate for all cases of the Polynesian routing simulation."""
